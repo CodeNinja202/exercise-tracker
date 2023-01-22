f1ba74cc5d53b8114e4719ae3bcbb7c65ba84370
Ross <rosskeene196@gmail.com>
2023-01-22 11:09:27 -0600

    complete

--- index.js
+++ index.js
@@ -140,6 +140,29 @@ app.get('/api/users', (req, res) => {
   })
 
 })
+
+app.use((req, res, next) => {
+	return next({ status: 404, message: 'not found' });
+});
+
+app.use((err, req, res, next) => {
+	let errCode, errMessage;
+
+	if (err.errors) {
+		// mongoose validation error
+		errCode = 400; // bad request
+		const keys = Object.keys(err.errors);
+		// report the first validation error
+		errMessage = err.errors[keys[0]].message;
+	} else {
+		// generic or custom error
+		errCode = err.status || 500;
+		errMessage = err.message || 'Internal Server Error';
+	}
+
+	res.status(errCode).type('txt')
+		.send(errMessage);
+});
 app.listen(PORT, () => {
   console.log(`Server is up and running on port ${PORT}`);
 });
